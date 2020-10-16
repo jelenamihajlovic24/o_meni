caa7ced2928e36b22fbf46acefce9b7b439be157
jelenamihajlovic24 <jmihajlovic84@gmail.com>
2020-10-16 13:22:13 +0200

    Some shortening in meta tags

--- js/myScript.js
+++ js/myScript.js
@@ -1,20 +1,19 @@
 // rođendansko odbrojavanje na rodjendanski_poklon.html
-var countDownDate = new Date("Feb 24, 2020 15:37:25").getTime();
+var countDownDate = new Date("Feb 24, 2021 15:37:25").getTime();
 
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   var now = new Date().getTime();
-    
+
   var distance = countDownDate - now;
-    
+
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
+
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "Srećan rođendan!";
@@ -27,11 +26,11 @@ function hourglass() {
   a = document.getElementById("pjescani_sat");
   a.innerHTML = "&#xf251;";
   setTimeout(function () {
-      a.innerHTML = "&#xf252;";
-    }, 1000);
+    a.innerHTML = "&#xf252;";
+  }, 1000);
   setTimeout(function () {
-      a.innerHTML = "&#xf253;";
-    }, 2000);
+    a.innerHTML = "&#xf253;";
+  }, 2000);
 }
 hourglass();
 setInterval(hourglass, 3000);
